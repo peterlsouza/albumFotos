35fe32bcfc7990a3ffa9b8afd8b2764f1fc4c5e4
Peterson Luiz de Souza <peterson.souza@sage.com>
2021-06-20 17:09:30 -0300

    Módul finalizado

---
+++ AlbumFotos/Models/MyDbContext.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AlbumFotos.Models
+{
+    public class MyDbContext : DbContext
+    {
+        public MyDbContext(DbContextOptions<MyDbContext>options) : base(options)
+        {
+            ChangeTracker.QueryTrackingBehavior = QueryTrackingBehavior.NoTracking;
+            ChangeTracker.AutoDetectChangesEnabled = false;
+        }
+
+        public DbSet<Album> Albuns { get; set; }
+        public DbSet<Imagem> Imagens { get; set; }
+
+    }
+}
---
+++ AlbumFotos/ViewComponents/ImagemViewComponent.cs
@@ -0,0 +1,25 @@
+﻿using AlbumFotos.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AlbumFotos.ViewComponents
+{
+    public class ImagemViewComponent : ViewComponent
+    {
+        private readonly MyDbContext _context;
+
+        public ImagemViewComponent(MyDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<IViewComponentResult> InvokeAsync(int id)
+        {
+            return View(await _context.Imagens.Where(x => x.AlbumId == id).ToListAsync());
+        }
+    }
+}
